b499b491262138bb73fc6083162e9fd952b4bdf2
prostiak2019 <yarovenkodmv@gmail.com>
2020-07-01 20:56:50 +0300

    REACT_TS_homework: add css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import FirstTask from './tasks/FirstTask/FirstTask';
 
 function App() {
   return (
-    <div >
-     start
+    <div>
+      <FirstTask/>
     </div>
   );
 }
---
+++ src/tasks/FirstTask/FirstTask.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import s from './FirstTask.module.css'
+
+// console.log(s)
+const FirstTask = ( ) => {
+  return (
+    <div className={s.messageConteiner}>
+      FirstTask
+    </div>
+  )
+}
+
+export default FirstTask;
